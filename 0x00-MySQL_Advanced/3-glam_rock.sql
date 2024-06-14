5d2d8e9fe58ef642c82b85b90c7126fc8d795efd
Mohamed Gharieb <m.gharieb89@gmail.com>
2024-06-14 02:00:02 +0000

    t2v0

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,6 @@
 -- script that ranks country of bands,
 -- ordered by the number of (non-unique) fans
-SELECT origin, SUM(fans) as nb_fans
+SELECT origin, SUM(fans) as fans_count
 FROM metal_bands
 GROUP BY origin
-ORDER BY nb_fans DESC;
+ORDER BY fans_count DESC;
