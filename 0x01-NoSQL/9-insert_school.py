dc101c7c92b20a68c031a1caac71b939c59bf5ad
Mohamed Gharieb <m.gharieb89@gmail.com>
2024-06-21 23:44:55 +0000

    t9v0

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+for insert docs in python
+"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """
+    that inserts a new document in a collection based on kwargs
+    """
+    new_docs = mongo_collection.insert_one(**kwargs)
+    return new_docs.inserted_id
