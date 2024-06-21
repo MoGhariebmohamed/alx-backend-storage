8c849f2fb8dd325259c9890a5b9e5ec07cb46cd5
Mohamed Gharieb <m.gharieb89@gmail.com>
2024-06-21 23:56:17 +0000

    t10v4

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -8,4 +8,4 @@ def update_topics(mongo_collection, name, topics):
     """
     changes all topics of a school document
     """
-    mongo_collection.update_many({"name": name, {"$set": {"topice": topics}})
+    mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
