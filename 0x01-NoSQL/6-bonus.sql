360321a5b6a8c9545c69e5215c63718ab68287db
Mohamed Gharieb <m.gharieb89@gmail.com>
2024-06-21 16:19:21 +0000

    t0v3

--- 0x01-NoSQL/6-bonus.sql
+++
@@ -1,7 +0,0 @@
--- script that ranks country of bands,
--- ordered by the number of (non-unique) fans
-CREATE TRIGGER decrement
-AFTER INSERT
-ON orders
-FOR EACH ROW
-UPDATE items SET quantity = quantity - NEW.number WHERE NAME = NEW.item_name;
